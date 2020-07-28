09450c26ecf1e3e730fd35e5b0883d236a14f68b
Calewiel <34463226+Calewiel@users.noreply.github.com>
2020-07-27 20:48:19 -0400

    Update heartfailure_v1.m
    Code included to clean the data and remove rows. Step 1 complete

--- heartfailure_v1.m
+++ heartfailure_v1.m
@@ -1,13 +1,26 @@
 data = readmatrix("heart_failure_clinical_records_dataset.csv");
 
-for i = 1:length(data)
-    if data(i,3) > 999
-        data(i,:) = [];
-    end
+data(:,7) = round(data(:,7));
+
+indices = find(data(:,3) > 999);
+data(indices, :) = [];
+
+indices = find(data(:,8) > 5);
+data(indices,:) = [];
+
+
+%creating a scatter plot between age and platlets count. We can see from
+%the data that people between 45-73(approx) have the highest platlet count.
+%But we can also see from the graph that most of the people have platlet
+%count between 1 to 3.5(approx).
+scatter(data(:,1), data(:,7));
+
+
+
+
+cor_matrix = [data(:,13) data(:,3) data(:,9)];
+
+indices = find(cor_matrix(:,1)==0);
+
+cor_matrix(indices,:) = [];
     
-    data(i,7) = round(data(i,7));
-    
-    if data(i,8) > 5
-    data(i,:) = [];
-    end
-end

cb8d0c0c944aa21c3da5e87c86b4ea651f35826e
Calewiel <34463226+Calewiel@users.noreply.github.com>
2020-08-05 19:07:50 -0400

    Update heartfailure_v1.m

--- heartfailure_v1.m
+++ heartfailure_v1.m
@@ -22,13 +22,36 @@
 
 %_________________________________________________________________________________________________________________________
 
+%In this project we have data for heart failure patients. some of them died
+%and the others did not. we are presented with different types of data for
+%the said patients e.g. if they had diabetes or not, how old they were, if
+%they had high blood pressure or not etc. 
+
+%Our group will be first cleaning the data to remove the invalid or
+%illogical values. If we find a value in any row invalid we are going to
+%remove the entire row since that data and any respective data can not be
+% used in our analysis.
+
+%importing the excel data file into matlab. the reason why we used
+%readtable and not readmatrix is because we wanted the column headers and
+%wanted to be able to reference a column with the data name.
+
 data = readtable("heart_failure_clinical_records_dataset.csv");
 
-data.platelets = floor(data.platelets);
+%rounding the age and platelets columns to int values.
 
+data.age = round(data.age);
+data.platelets = round(data.platelets);
+
+%here we are cleaning the serum column data by finding any value which is
+%greater than 5 since it looks invalid and then we are removing the whole
+%row which had that invalid value.
 indices = find(data.serum_creatinine > 5);
 data(indices,:) = [];
 
+%here we are cleaning the creatinine column data by finding any value which 
+%is greater than 999 since it looks invalid and then we are removing the 
+%whole row which had that invalid value.
 indices = find(data.creatinine_phosphokinase > 999);
 data(indices, :) = [];
 
@@ -43,9 +66,72 @@ scatter(data.age, data.platelets);
 
 
 
-cor_matrix = [data.DEATH_EVENT data.creatinine_phosphokinase data.serum_sodium];
+%we are creating a variable called "cor_matrix" which we will use now and
+%also later in the project to store filtered data so that we can compare
+%and analyse the filtered data properly. Now we are creating a matrix with
+%"death event, age and smoking"
+cor_matrix = [data.DEATH_EVENT data.age data.smoking];
 
+%we are finding all the people who are not dead and removing all the data
+%from the matrix for the alive people.
 indices = find(cor_matrix(:,1)==0);
-
 cor_matrix(indices,:) = [];
-    
+
+%removing the column which shows the binary value for people being dead. We
+%dont need this column anymore. 
+cor_matrix(:,1) = [];
+
+%scatter plot between age and smoking. 
+scatter(cor_matrix(:,1),cor_matrix(:,2))
+xlabel("Age");
+ylabel("Smoking(0 = No, 1 = Yes)");
+
+
+%creating a boxchart between age and smoking
+boxchart(cor_matrix(:,2),cor_matrix(:,1))
+xlabel("Smoking(0 = No, 1 = Yes)");
+ylabel("Age");
+
+
+% ANALYSIS %
+
+%We did not find any direct relation between age, smoking and the people
+%who died. This data is for people who are dead and the data shows that
+%people who smoked almost lived as long as the people who did not and
+%suffered heart failure. 
+
+%Now we are creating a matrix with "death event, age and smoking"
+cor_matrix = [data.DEATH_EVENT data.age data.smoking];
+
+%we are finding all the people who are dead and removing all the data
+%from the matrix for the dead people.
+indices = find(cor_matrix(:,1)==1);
+cor_matrix(indices,:) = [];
+
+%removing the column which shows the binary value for people being not 
+% dead. We dont need this column anymore. 
+cor_matrix(:,1) = [];
+
+
+%scatter plot between age and smoking. 
+scatter(cor_matrix(:,1),cor_matrix(:,2))
+xlabel("Age");
+ylabel("Smoking(0 = No, 1 = Yes)");
+
+
+%creating a boxchart between age and smoking
+boxchart(cor_matrix(:,2),cor_matrix(:,1))
+xlabel("Smoking(0 = No, 1 = Yes)");
+ylabel("Age");
+
+% ANALYSIS %
+
+%We are using the same table elements but this time we are comparing
+%smoking to age for the people who are alive. This data shows us that
+%people who did not smoke got to live a bit more than the people who smoked
+%and suffered heart failure. The age group for the data remains somewhat
+%consistent in both instances. 
+
+
+
+%_________________________________________________________________

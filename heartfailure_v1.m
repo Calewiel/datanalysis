714b41a2fe33033e2c361e70e9f44cfa0e733fd1
Calewiel <34463226+Calewiel@users.noreply.github.com>
2020-08-11 23:42:44 -0400

    Update heartfailure_v1.m

--- heartfailure_v1.m
+++ heartfailure_v1.m
@@ -1,151 +1,125 @@
-%In this project we have data for heart failure patients. some of them died
-%and the others did not. we are presented with different types of data for
-%the said patients e.g. if they had diabetes or not, how old they were, if
-%they had high blood pressure or not etc. 
+% In this project we have data for heart failure patients. some of them died
+% and the others did not. we are presented with different types of data for
+% the said patients e.g. if they had diabetes or not, how old they were, if
+% they had high blood pressure or not etc. 
 
-%Our group will be first cleaning the data to remove the invalid or
-%illogical values. If we find a value in any row invalid we are going to
-%remove the entire row since that data and any respective data can not be
-%used in our analysis.
+% Our group will be first cleaning the data to remove the invalid or
+% illogical values. If we find a value in any row invalid we are going to
+% remove the entire row since that data and any respective data can not be
+% used in our analysis. What we are trying to figure out is what factors
+% contributed to the death event. E.g. if smoking was a factor in people who
+% died etc. 
 
-%importing the excel data file into matlab. the reason why we used
-%readtable and not readmatrix is because we wanted the column headers and
-%wanted to be able to reference a column with the data name.
+% importing the excel data file into matlab. the reason why we used
+% readtable and not readmatrix is because we wanted the column headers and
+% wanted to be able to reference a column with the data name.
 
 data = readtable("heart_failure_clinical_records_dataset.csv");
 
-%rounding the age and platelets columns to int values.
+% rounding the age and platelets columns to int values.
 
 data.age = round(data.age);
 data.platelets = round(data.platelets);
 
-%here we are cleaning the serum column data by finding any value which is
-%greater than 5 since it looks invalid and then we are removing the whole
-%row which had that invalid value.
+% here we are cleaning the serum column data by finding any value which is
+% greater than 5 since it looks invalid and then we are removing the whole
+% row which had that invalid value. We checked on Google and it states that
+% if serum_creatinine is above 5 it is super abnormal. 
 indices = find(data.serum_creatinine > 5);
 data(indices,:) = [];
 
-%here we are cleaning the creatinine column data by finding any value which 
-%is greater than 999 since it looks invalid and then we are removing the 
-%whole row which had that invalid value.
+% here we are cleaning the creatinine column data by finding any value which 
+% is greater than 999 since it looks invalid and then we are removing the 
+% whole row which had that invalid value. We checked on Google and it says
+% if creatinine_phosphokinase if over 1000 it is super abnormal. 
 indices = find(data.creatinine_phosphokinase > 999);
 data(indices, :) = [];
 
 
 % MINI ANALYSIS
-%creating a scatter plot between age and platlets count. We can see from
-%the data that people between 45-73(approx) have the highest platlet count.
-%But we can also see from the graph that most of the people have platlet
-%count between 1 to 3.5(approx).
+% creating a scatter plot between age and platlets count. We can see from
+% the data that people between 45-73(approx) have the highest platlet count.
+% But we can also see from the graph that most of the people have platlet
+% count between 1 to 3.5(approx).
 scatter(data.age, data.platelets);
 %_______________________________________________________%
 
 
-%we are creating a variable called "cor_matrix" which we will use now and
-%also later in the project to store filtered data so that we can compare
-%and analyse the filtered data properly. Now we are creating a matrix with
-%"death event, age and smoking"
+% we are creating a variable called "cor_matrix" which we will use now and
+% also later in the project to store filtered data so that we can compare
+% and analyse the filtered data properly. Now we are creating a matrix with
+% "death event, age and smoking"
 cor_matrix = [data.DEATH_EVENT data.age data.smoking];
 
-%we are finding all the people who are not dead and removing all the data
-%from the matrix for the alive people.
+% we are finding all the people who are not dead and removing all the data
+% from the matrix for the alive people.
 indices = find(cor_matrix(:,1)==0);
 cor_matrix(indices,:) = [];
 
-%removing the column which shows the binary value for people being dead. We
-%dont need this column anymore. 
+% removing the column which shows the binary value for people being dead. We
+% dont need this column anymore.
 cor_matrix(:,1) = [];
 
-%scatter plot between age and smoking. 
-scatter(cor_matrix(:,1),cor_matrix(:,2))
-xlabel("Age");
-ylabel("Smoking(0 = No, 1 = Yes)");
 
+diedSmoking = sum(cor_matrix(:,2)==1);
+diedNotSmoking = sum(cor_matrix(:,2)==0);
 
-%creating a boxchart between age and smoking
-boxchart(cor_matrix(:,2),cor_matrix(:,1))
-xlabel("Smoking(0 = No, 1 = Yes)");
-ylabel("Age");
-
-
-% ANALYSIS %
-
-%We did not find any direct relation between age, smoking and the people
-%who died. This data is for people who are dead and the data shows that
-%people who smoked almost lived as long as the people who did not and
-%suffered heart failure.
-%_______________________________________________________%
-
-%Now we are creating a matrix with "death event, age and smoking"
 cor_matrix = [data.DEATH_EVENT data.age data.smoking];
 
-%we are finding all the people who are dead and removing all the data
-%from the matrix for the dead people.
+% we are finding all the people who are not dead and removing all the data
+% from the matrix for the alive people.
 indices = find(cor_matrix(:,1)==1);
 cor_matrix(indices,:) = [];
-
-%removing the column which shows the binary value for people being not 
-% dead. We dont need this column anymore. 
 cor_matrix(:,1) = [];
 
+notDeadSmoking = sum(cor_matrix(:,2)==1);
+notDeadNotSmoking = sum(cor_matrix(:,2)==0);
 
-%scatter plot between age and smoking. 
-scatter(cor_matrix(:,1),cor_matrix(:,2))
-xlabel("Age");
-ylabel("Smoking(0 = No, 1 = Yes)");
-
-
-%creating a boxchart between age and smoking
-boxchart(cor_matrix(:,2),cor_matrix(:,1))
-xlabel("Smoking(0 = No, 1 = Yes)");
-ylabel("Age");
 
 % ANALYSIS %
 
-%We used the same table elements but this time we compared
-%smoking to age for the people who are alive. This data shows us that
-%people who did not smoke got to live a bit more than the people who smoked
-%and suffered heart failure. The age group for the data remains somewhat
-%consistent in both instances. 
+% We did not find any direct relation between age, smoking and the people
+% who died. As you can see from our calculations we compared the number of
+% people who died and did not die while smoking and who did not die while
+% smoking and not smoking. Our results show that the people's dead in this 
+% dataset do not have a direct relation with smoking. People who actually
+% died and did not smoke are higher in number than people who did smoke. So
+% according to this set, the myth/fact that smoking causes death is not
+% true.
 %_______________________________________________________%
 
-%creating a matrix with smoking and platelet count
-cor_matrix = [data.platelets data.smoking];
 
-%box charting the data.
-boxchart(cor_matrix(:,2),cor_matrix(:,1))
-xlabel("Smoking(0 = No, 1 = Yes)");
-ylabel("Platelet Count")
+platNotDead = mean(data.platelets(data.DEATH_EVENT == 0));
+platDead = mean(data.platelets(data.DEATH_EVENT == 1));
+
 
 % ANALYSIS % 
-% We found interesting results when we compared platelets and smoking.
-% Most of the people who did not smoke had platelet count between 200k to
-% 300k but there were a lot of outliers on the high end and low end. But we
-% did not see any outliers on the low end and very few on the high end for
-% the people who smoked. This shows that smoking effects your creatinine
-% levels and prevents them from increasing and decreasing significantly. 
+% We compared the platelet count for the people who died and who didn't but
+% did not find any significant difference in the values. From this we can
+% conclude that the platelet count does not have any impact on people who 
+% suffered the death event.
 %_______________________________________________________%
 
+cor_matrix = [data.DEATH_EVENT data.diabetes];
+notDeadHadDiab = sum(cor_matrix(:,1)==0 & cor_matrix(:,2)==1);
+notDeadNoDiab = sum(cor_matrix(:,1)==0 & cor_matrix(:,2)==0);
 
-%creating a matrix with age and diabetes
-cor_matrix = [data.age data.diabetes];
+deadHadDiab = sum(cor_matrix(:,1)==1 & cor_matrix(:,2)==1);
+deadNoDiab =sum(cor_matrix(:,1)==1 & cor_matrix(:,2)==0);
 
-%box charting the data.
-boxchart(cor_matrix(:,2),cor_matrix(:,1))
-xlabel("Diabetes (0 = No, 1 = Yes");
-ylabel("Age")
 
 % ANALYSIS %
-%This comparison shows that people who dont have diabetes have a higher age
-%range than the ones who do. This could be because diabetes causes other
-%health problems is also a factor in heart failure. There are a few
-%outliers in people who smoked which were lucky enough to make it long age.
+% We find from the data that is no direct relation between having diabetes
+% and death because the people who had diabetes and died is almost the same
+% as people who died and did not have diabetes. Our conclusion is that in
+% the event of a heart failure both people with and without diabetes have
+% the same probabibility to die. 
 %_______________________________________________________%
 
 
-%we are creating a box chart here between age and creatinine. We check the
-%min and max values of age so that we can categorize the bins accordingly.
-%we give labels to the x axis bins and catergorize the bins in 5. 
+% we are creating a box chart here between age and creatinine. We check the
+% min and max values of age so that we can categorize the bins accordingly.
+% we give labels to the x axis bins and catergorize the bins in 5. 
 
 min(data.age);
 max(data.age);
@@ -156,14 +130,15 @@ boxchart(groupAge,data.creatinine_phosphokinase)
 
 % ANALYSIS %
 
-%We are unable to make any concrete relation or analysis between age and
-%creatinine as creatinine levels seem consistent in all groups. The only
-%thing that stands out is people between the age of 60 and 80 seem to have
-%lower creatinine levels than the others. 
+% We were trying to figure out what relation is between age and creatinine
+% levels and how it would effect the death event but we were unable to find
+% any substansive change in data within different age groups. Creatinine
+% levels seems to be consistent throughout hence proving that creatinine
+% levels dont increase or decrease with age. 
 %_______________________________________________________%
 
-%here we are going to compare age and ejection fraction. We use the same
-%method as before.
+% here we are going to compare age and ejection fraction. We use the same
+% method as before.
 binEdges = 40:15:100;
 bins = {'40s','50s','60s','70s','80s+'};
 groupAge = discretize(data.age,5,"categorical",bins);
@@ -171,32 +146,33 @@ boxchart(groupAge,data.ejection_fraction)
 
 % ANALYSIS % 
 
-%This data is actually more non consistent than before which gives us an
-%idea of comparison. The ejection fraction is increasing as the age
-%increases. We can also see that people between the age of 60 and 70 seem
-%to have the highest fluctuation in their ejection fraction than the other
-%age groups. 
+% We tried to analyze a relation between age and ejection fraction but we
+% were again unable to find any relation. The data seems mostly consistent.
 %_______________________________________________________%
 
-%this fetches all the averages for the different non-binary factors that
-%are within the dataset and then we analyze the data of the people that died 
-%by comparing their total averages to those ofliving people and total average to see 
-%which factors are predominantly above average which will indicate a correlation with death  
+% this fetches all the averages for the different non-binary factors that
+% are within the dataset and then we analyze the data of the people that died 
+% by comparing their total averages to those ofliving people and total average to see 
+% which factors are predominantly above average which will indicate a correlation with death  
 
-totalAverages = [mean(data.age) mean(data.creatinine_phosphokinase) mean(data.ejection_fraction) mean(data.high_blood_pressure) mean(data.platelets) mean(data.serum_creatinine) mean(data.serum_sodium)];
-averagesOfDead = [mean(data.age(data.DEATH_EVENT > 0)) mean(data.creatinine_phosphokinase(data.DEATH_EVENT > 0)) mean(data.ejection_fraction(data.DEATH_EVENT > 0)) mean(data.high_blood_pressure(data.DEATH_EVENT > 0)) mean(data.platelets(data.DEATH_EVENT > 0)) mean(data.serum_creatinine(data.DEATH_EVENT > 0)) mean(data.serum_sodium(data.DEATH_EVENT > 0))];
-averagesOfLiving = [mean(data.age(data.DEATH_EVENT == 0)) mean(data.creatinine_phosphokinase(data.DEATH_EVENT == 0)) mean(data.ejection_fraction(data.DEATH_EVENT == 0)) mean(data.high_blood_pressure(data.DEATH_EVENT == 0)) mean(data.platelets(data.DEATH_EVENT == 0)) mean(data.serum_creatinine(data.DEATH_EVENT == 0)) mean(data.serum_sodium(data.DEATH_EVENT == 0))];
+totalAverages = [mean(data.age) mean(data.creatinine_phosphokinase) mean(data.ejection_fraction) mean(data.platelets) mean(data.serum_creatinine) mean(data.serum_sodium)];
+averagesOfDead = [mean(data.age(data.DEATH_EVENT > 0)) mean(data.creatinine_phosphokinase(data.DEATH_EVENT > 0)) mean(data.ejection_fraction(data.DEATH_EVENT > 0)) mean(data.platelets(data.DEATH_EVENT > 0)) mean(data.serum_creatinine(data.DEATH_EVENT > 0)) mean(data.serum_sodium(data.DEATH_EVENT > 0))];
+averagesOfLiving = [mean(data.age(data.DEATH_EVENT == 0)) mean(data.creatinine_phosphokinase(data.DEATH_EVENT == 0)) mean(data.ejection_fraction(data.DEATH_EVENT == 0)) mean(data.platelets(data.DEATH_EVENT == 0)) mean(data.serum_creatinine(data.DEATH_EVENT == 0)) mean(data.serum_sodium(data.DEATH_EVENT == 0))];
 
 plotMat = [totalAverages; averagesOfDead; averagesOfLiving]
 
 
 % ANALYSIS %
-%We just compared the averages of the original data set for the non binary
-%values with the average of dead and living respectively. We can see that
-%average of the living is actually much lower than the others and their
-%average of the creatinine levels is also lower than the others. This
-%comparison doesnt really give us any information but we wanted to just
-%have averages for reference. 
+% We just compared the averages of the original data set for the non binary
+% values with the average of dead and living respectively. We can see that
+% average of the living is actually much lower than the others and their
+% average of the creatinine levels is also lower than the others. We also
+% noticed that the average age of the dead is higher than that of the
+% living. The one main significant difference is in serum creatinine(SC). The
+% people who died seem to have a 50% higher SC than the one who are living.
+% This shows us that high SC is likely a direct factor for causing a death
+% event if heart failure happens. 
+
 %_______________________________________________________%
 
 
@@ -208,25 +184,25 @@ males = mean(data.age(data.sex == 1 & data.smoking == 1 & data.diabetes == 1 & d
 females = mean(data.age(data.sex == 0 & data.smoking == 1 & data.diabetes == 1 & data.high_blood_pressure == 1 & data.DEATH_EVENT == 1))
 
 % ANALYSIS % 
-%males = 68
-%females = 50
-%this shows that the average of the dead men who smoked, had diabetes and
-%high blood pressure is higher than that of women. This data proves that
-%men with the above mentioned conditions live longer than women with the
-%same conditions. 
+% males = 68
+% females = 50
+% this shows that the average of the dead men who smoked, had diabetes and
+% high blood pressure is higher than that of women. This data proves that
+% men with the above mentioned conditions live longer than women with the
+% same conditions. 
 %_______________________________________________________%
 
 
-%same as above but for people who didn't die.
+% same as above but for people who didn't die.
 notDeadMales = mean(data.age(data.sex == 1 & data.smoking == 1 & data.diabetes == 1 & data.high_blood_pressure == 1 & data.DEATH_EVENT == 0))
 notDeadFemales = mean(data.age(data.sex == 0 & data.smoking == 1 & data.diabetes == 1 & data.high_blood_pressure == 1 & data.DEATH_EVENT == 0))
 
 % ANALYSIS %
-%notDeadMales = 68
-%notDeadFemales = 0
-%this data shows that women who smoked, had diabetes and high blood
-%pressure all died in the event of a heart failure where as 68 men survived
-%who had the same conditions. 
+% notDeadMales = 68
+% notDeadFemales = 0
+% this data shows that women who smoked, had diabetes and high blood
+% pressure all died in the event of a heart failure where as 68 men survived
+% who had the same conditions. 
 %_______________________________________________________%
 
 
@@ -238,17 +214,17 @@ countNotDeadMales = sum(data.sex == 1 & data.smoking == 1 & data.diabetes == 1 &
 countNotDeadFemales = sum(data.sex == 0 & data.smoking == 1 & data.diabetes == 1 & data.high_blood_pressure == 1 & data.DEATH_EVENT == 0);
 
 % ANALYSIS %
-%countMales = 4
-%countFemales = 1
-%countNotDeadMales = 6
-%countNotDeadFemales = 0
+% countMales = 4
+% countFemales = 1
+% countNotDeadMales = 6
+% countNotDeadFemales = 0
 
-%we calculated the number of males and females who died and had all the
-%binary health conditions in the data set. More males died than females.
-%But also more males survived than females in this data set. Through this
-%we can conclude that the number of males is higher in this data set than
-%women, also that women have a higher mortality rate than men with the
-%above mentioned health conditions. 
+% we calculated the number of males and females who died and had all the
+% binary health conditions in the data set. More males died than females.
+% But also more males survived than females in this data set. Through this
+% we can conclude that the number of males is higher in this data set than
+% women, also that women have a higher mortality rate than men with the
+% above mentioned health conditions. 
 %_______________________________________________________%
 
 
